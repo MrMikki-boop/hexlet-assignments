0783b1d80171cde6446ce0d9cdf5396ed6276a34
@hexlet/cli <support@hexlet.io>
2023-08-01 23:47:38 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,44 +6,37 @@ import java.util.Map;
 // BEGIN
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
-        // Создаем новый словарь для хранения слов и их количества
         Map<String, Integer> wordCountMap = new HashMap<>();
 
-        // Разбиваем предложение на слова, используя пробел в качестве разделителя
+        // Разделяем предложение на слова по пробелам
         String[] words = sentence.split("\\s+");
 
-        // Обрабатываем каждое слово
+        // Подсчитываем количество каждого слова
         for (String word : words) {
-            // Если слово уже есть в словаре, увеличиваем его количество на 1
-            if (wordCountMap.containsKey(word)) {
-                int count = wordCountMap.get(word);
-                wordCountMap.put(word, count + 1);
-            } else {
-                // Если слова нет в словаре, добавляем его со значением 1
-                wordCountMap.put(word, 1);
-            }
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
         }
 
         return wordCountMap;
     }
 
     public static String toString(Map<String, Integer> wordCountMap) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("{\n");
+        StringBuilder sb = new StringBuilder("{\n");
         for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
-            sb.append("  ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            sb.append("  ").append(entry.getKey()).append("=").append(entry.getValue()).append(",\n");
         }
-        sb.append("}");
+        // Удаляем запятую после последней пары ключ-значение
+        if (!wordCountMap.isEmpty()) {
+            sb.setLength(sb.length() - 2);
+        }
+        sb.append("\n}");
 
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        String sentence = "Lorem ipsum dolor sit amet consectetur adipiscing elit";
-        Map<String, Integer> wordCountMap = getWordCount(sentence);
-        String result = toString(wordCountMap);
-        System.out.println(result);
+        String sentence = "java is the best programming language java";
+        Map<String, Integer> wordsCount = App.getWordCount(sentence);
+        System.out.println(App.toString(wordsCount));
     }
 }
 //END
